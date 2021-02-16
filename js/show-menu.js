9ef4dc8093faf2f0cb857e0ba12a71705b37afab
Sergejs-Vasjkovs <s.vasjkovs@gmail.com>
2021-02-16 19:11:40 +0200

    complete desktop adaptive

--- js/show-menu.js
+++ js/show-menu.js
@@ -1,19 +1,31 @@
 var buttonMenu = document.querySelector(".button-menu");
 var menu = document.querySelector(".menu__drop-down");
 var closeMenu = document.querySelector(".close-button");
+var darkFIlter = document.querySelector(".page-brand");
 
 buttonMenu.addEventListener("click", function (evt) {
     evt.preventDefault();
-    if (menu.classList.contains("menu-modal--show")) {
-        menu.classList.remove("menu-modal--show");
+    if (!document.body.classList.contains(darkFIlter)) {
+        if (menu.classList.contains("menu-modal--show")) {
+            menu.classList.remove("menu-modal--show");
+            darkFIlter.classList.remove("page-brand--dark-mode");
+        } else {
+            menu.classList.add("menu-modal--show");
+            darkFIlter.classList.add("page-brand--dark-mode");
+        }
     } else {
-        menu.classList.add("menu-modal--show");
+        if (menu.classList.contains("menu-modal--show")) {
+            menu.classList.remove("menu-modal--show");
+        } else {
+            menu.classList.add("menu-modal--show");
+        }
     }
 });
 
 closeMenu.addEventListener("click", function (evt) {
     evt.preventDefault();
     menu.classList.remove("menu-modal--show");
+    darkFIlter.classList.remove("page-brand--dark-mode");
 });
 
 window.addEventListener("keydown", function (evt) {
@@ -21,6 +33,7 @@ window.addEventListener("keydown", function (evt) {
         if (menu.classList.contains("menu-modal--show")) {
             evt.preventDefault();
             menu.classList.remove("menu-modal--show");
+            darkFIlter.classList.remove("page-brand--dark-mode");
         }
     }
 });
@@ -48,4 +61,100 @@ window.addEventListener("keydown", function (evt) {
         }
     }
 });
+
+
+// Filters
+//trends
+var buttonTrends = document.getElementById("trends");
+var trend = document.getElementById("trends--list");
+
+buttonTrends.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    if (trend.classList.contains("filter__list--show")) {
+        trend.classList.remove("filter__list--show");
+    } else {
+        trend.classList.add("filter__list--show");
+    }
+});
+
+window.addEventListener("keydown", function (evt) {
+    if (evt.keyCode === 27) {
+        if (trend.classList.contains("filter__list--show")) {
+            evt.preventDefault();
+            trend.classList.remove("filter__list--show");
+        }
+    }
+});
+
+window.addEventListener("mouseup", function (evt) {
+    if (document.getElementById("trends--list").contains(evt.target)) {
+    } else {
+        if (trend.classList.contains("filter__list--show")) {
+            trend.classList.remove("filter__list--show");
+        }
+    }
+});
+
+//size
+var buttonFilter = document.getElementById("size");
+var filter = document.getElementById("size--list");
+
+buttonFilter.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    if (filter.classList.contains("filter__list--show")) {
+        filter.classList.remove("filter__list--show");
+    } else {
+        filter.classList.add("filter__list--show");
+    }
+});
+
+window.addEventListener("keydown", function (evt) {
+    if (evt.keyCode === 27) {
+        if (filter.classList.contains("filter__list--show")) {
+            evt.preventDefault();
+            filter.classList.remove("filter__list--show");
+        }
+    }
+});
+
+window.addEventListener("mouseup", function (evt) {
+    if (document.getElementById("size--list").contains(evt.target)) {
+    } else {
+        if (filter.classList.contains("filter__list--show")) {
+            filter.classList.remove("filter__list--show");
+        }
+    }
+});
+
+//price
+var buttonPrice = document.getElementById("price");
+var price = document.getElementById("price--list");
+
+buttonPrice.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    if (price.classList.contains("filter__list--show")) {
+        price.classList.remove("filter__list--show");
+    } else {
+        price.classList.add("filter__list--show");
+    }
+});
+
+window.addEventListener("keydown", function (evt) {
+    if (evt.keyCode === 27) {
+        if (price.classList.contains("filter__list--show")) {
+            evt.preventDefault();
+            price.classList.remove("filter__list--show");
+        }
+    }
+});
+
+window.addEventListener("mouseup", function (evt) {
+    if (document.getElementById("price--list").contains(evt.target)) {
+    } else {
+        if (price.classList.contains("filter__list--show")) {
+            price.classList.remove("filter__list--show");
+        }
+    }
+});
+
 
